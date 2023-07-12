88b40faa388b825c28e7e87f86f394e123947c4e
quantum <quantum@quantums-Mac-mini.local>
2023-07-12 00:00:57 -0400

    sma

--- src/ta/ta.c
+++ src/ta/ta.c
@@ -1,124 +1,74 @@
-
-#include <math.h>
-#include <stddef.h>
+#include <stdio.h>
+// #include <sqlite3.h>
 #include <sqlite3ext.h>
 
-
-SQLITE_EXTENSION_INIT3
-
-typedef struct MovingAvgContext {
-  int windowSize;
+typedef struct {
   int count;
+  int window;
   double sum;
   double *values;
-} MovingAvgContext;
-
-typedef struct EMACtx {
-  double alpha;
-  double prevEMA;
-  int count;
-} EMACtx;
+} WindowData;
 
 static void smaStep(sqlite3_context *context, int argc, sqlite3_value **argv) {
-  MovingAvgContext *maCtx;
+  if (argc != 2) {
+    sqlite3_result_error(context, "Invalid number of arguments. Two arguments are required.", -1);
+    return;
+  }
 
-  if (sqlite3_user_data(context) == NULL) {
-    // 初始化窗口上下文
-    maCtx = sqlite3_malloc(sizeof(MovingAvgContext));
-    maCtx->windowSize = 0;
-    maCtx->count = 0;
-    maCtx->sum = 0;
-    maCtx->values = NULL;
+  WindowData *windowData = sqlite3_aggregate_context(context, sizeof(WindowData));
 
-    sqlite3_result_pointer(context, maCtx, "MovingAvgContext", NULL);
+  if (windowData->values == NULL) {
+    windowData->window = sqlite3_value_int(argv[1]);
+    windowData->count = 0;
+    windowData->sum = 0;
+    windowData->values = sqlite3_malloc(sizeof(double) * windowData->window);
+  }
+
+  double value = sqlite3_value_double(argv[0]);
+
+  if (windowData->count < windowData->window) {
+    windowData->values[windowData->count] = value;
+    windowData->sum += value;
+    windowData->count++;
   } else {
-    maCtx = (MovingAvgContext *)sqlite3_value_pointer(argv[0], "EMACtx");
+    int index=windowData->count%windowData->window;
+    windowData->sum -= windowData->values[index];
+    windowData->values[index] = value;
+    windowData->sum += value; 
 
-    if (maCtx->count < maCtx->windowSize) {
-      double currentValue = sqlite3_value_double(argv[0]);
+    double sma = windowData->sum / windowData->window;
 
-      maCtx->sum += currentValue;
-      maCtx->values[maCtx->count] = currentValue;
-      maCtx->count++;
-    } else {
-      double currentValue = sqlite3_value_double(argv[0]);
-      double oldestValue = maCtx->values[maCtx->count % maCtx->windowSize];
-
-      maCtx->sum = maCtx->sum - oldestValue + currentValue;
-      maCtx->values[maCtx->count % maCtx->windowSize] = currentValue;
-      maCtx->count++;
-    }
+    sqlite3_result_double(context, sma);
   }
 }
 
-static void smaFinal(sqlite3_context *context) {
-  MovingAvgContext *maCtx = (MovingAvgContext *)sqlite3_value_pointer(sqlite3_context_value(context), "EMACtx");
 
-  if (maCtx != NULL && maCtx->count > 0) {
-    sqlite3_result_double(context, maCtx->sum / maCtx->count);
-  }
+static void smaFinalize(sqlite3_context *context) {
+  WindowData *windowData = sqlite3_aggregate_context(context, sizeof(WindowData));
+  sqlite3_free(windowData->values);
 }
 
-static void smaValueDestructor(void *p) {
-  MovingAvgContext *maCtx = (MovingAvgContext *)p;
-
-  if (maCtx != NULL) {
-    sqlite3_free(maCtx->values);
-    sqlite3_free(maCtx);
-  }
-}
-
-static void emaStep(sqlite3_context *context, int argc, sqlite3_value **argv) {
-  EMACtx *emaCtx;
-
-  if (sqlite3_user_data(context) == NULL) {
-    // 初始化上下文
-    emaCtx = sqlite3_malloc(sizeof(EMACtx));
-    emaCtx->alpha = 2.0 / (sqlite3_value_double(argv[0]) + 1.0);
-    emaCtx->prevEMA = 0.0;
-    emaCtx->count = 0;
-
-    sqlite3_result_pointer(context, emaCtx, "EMACtx", NULL);
-  } else {
-    emaCtx = (EMACtx *)sqlite3_value_pointer(argv[0], "EMACtx");
-
-    if (emaCtx->count == 0) {
-      // 第一个值为初始EMA
-      emaCtx->prevEMA = sqlite3_value_double(argv[1]);
-    } else {
-      double currentValue = sqlite3_value_double(argv[1]);
-
-      // 计算当前EMA
-      double ema = (currentValue * emaCtx->alpha) + (emaCtx->prevEMA * (1 - emaCtx->alpha));
-      emaCtx->prevEMA = ema;
-    }
-
-    emaCtx->count++;
-  }
-}
-
-static void emaFinal(sqlite3_context *context) {
-  EMACtx *emaCtx = (EMACtx *)sqlite3_value_pointer(sqlite3_context_value(context), "EMACtx");
 
 
-  if (emaCtx != NULL && emaCtx->count > 0) {
-    sqlite3_result_double(context, emaCtx->prevEMA);
-  }
-}
-
-static void emaValueDestructor(void *p) {
-  EMACtx *emaCtx = (EMACtx *)p;
-
-  if (emaCtx != NULL) {
-    sqlite3_free(emaCtx);
-  }
-}
-
-int sqlite3_extension_init(sqlite3 *db, char **errmsg, const sqlite3_api_routines *api) {
+int sqlite3_extension_init(sqlite3 *db, char **err, const sqlite3_api_routines *api) {
   SQLITE_EXTENSION_INIT2(api);
 
-  sqlite3_create_window_function(db, "sma", 1, SQLITE_UTF8, NULL, smaStep, smaFinal, NULL,NULL, smaValueDestructor);
-  sqlite3_create_window_function(db, "ema", 2, SQLITE_UTF8, NULL, emaStep, emaFinal, NULL,NULL, emaValueDestructor);
+  int rc = sqlite3_create_window_function(
+    db,
+    "sma",
+    2,
+    SQLITE_UTF8 | SQLITE_DETERMINISTIC,
+    NULL,
+    smaStep,
+    smaFinalize,
+    NULL,
+    NULL,
+    NULL
+  );
+  if (rc != SQLITE_OK) {
+    *err = sqlite3_mprintf("Failed to create 'sma' function: %s", sqlite3_errmsg(db));
+    return rc;
+  }
 
   return SQLITE_OK;
 }
